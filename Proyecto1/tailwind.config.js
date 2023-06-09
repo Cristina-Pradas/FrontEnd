b01dcc64bc4e93dfb72f716877721c85c84362e5
Cristina-Pradas <127886495+Cristina-Pradas@users.noreply.github.com>
2023-06-09 13:42:11 +0200

    Add files via upload

---
+++ Proyecto1/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./input.css"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'Rubik':['Rubik','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+};
